e71fc9f1d27bd45902bc5792ff5ffba557fe858f
Ophrah Adderley <ophrah.adderley@simplybusiness.com>
2023-10-13 11:25:04 -0400

    edits to goals 2,3 & 4

--- shuffle_string.rb
+++ shuffle_string.rb
@@ -32,20 +32,58 @@ Goal1: Psuedocode
 5. Create a for loop iterating over indices
 6. characters = s[indices[0]] = s[4]
 7. Append character to the result variable
-8. Use the join function to make the result variable outputone word
+8. Add 1 to the counter in the loop
+9. Use the join function to make the result variable output one word
 '''
 
 '''
 Goal2:
-1. result is equal to an empty array and string s ="codeleet" and indices = [4,5,6,7,0,2,1,3]
+1. result is equal to an empty array, string s ="codeleet" and indices = [4,5,6,7,0,2,1,3]
 2. n = 0
 2. counter = 0
 3. If length of s and indices are equal, n = length of s
 4. if 1<= n <=100 true # learn about guard statements
-    5.f or each variable in indices
-    6. characters = "l"
-    7. use the append function on the result, result=["l"]
-    8. add one to the counter, counter = 1
+    5.for each variable in indices
+        6. character = "l"
+        7. use the append function on the result, result=["l"]
+        8. add one to the counter, counter = 1
+        9. using the join function, result =[l]
+    5. for each variable in indices
+        6. character = "e"
+        7. use the append function on the result, result=["e"]
+        8. add one to the counter, counter = 2
+        9. using the join function, result =[le]
+    5. for each variable in indices
+        6. character ="e"
+        7. use the append function on the result, result=["e"]
+        8. add one to the counter, counter = 3
+        9. using the join function, result =[lee]
+    5. for each variable in indices
+        6. character ="t"
+        7. use the append function on the result, result=["t"]
+        8. add one to the counter, counter = 4
+        9. using the join function, result =[leet]
+    5. for each variable in indices
+        6. character ="c"
+        7. use the append function on the result, result=["c"]
+        8. add one to the counter, counter = 5
+        9. using the join function, result =[leetc]
+    5. for each variable in indices
+        6. character ="o"
+        7. use the append function on the result, result=["o"]
+        8. add one to the counter, counter = 6
+        9. using the join function, result =[leetco]
+    5. for each variable in indices
+        6. character ="d"
+        7. use the append function on the result, result=["d"]
+        8. add one to the counter, counter = 7
+        9. using the join function, result =[leetcod]
+    5. for each variable in indices
+        6. character ="e"
+        7. use the append function on the result, result=["e"]
+        8. add one to the counter, counter = 8
+        9. using the join function, result =[leetcode]
+
 '''
 
 '''
@@ -54,7 +92,7 @@ Goals3&4:
 result = []
 n=0
 s="codeleet"
-indices = [4,5,6,7,0,2,1,3]
+indices = [4,5,6,7,0,1,2,3]
 if s.length == indices.length
     for x in indices do
         n+=1
