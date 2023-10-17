48c0e5b88ba00335e618a820a541133ec3ddf1a4
Ophrah Adderley <ophrah.adderley@simplybusiness.com>
2023-10-17 11:45:24 -0400

    added tests for base seven

--- base_seven_spec.rb
+++ base_seven_spec.rb
@@ -2,12 +2,18 @@
 
 describe "base seven representation of numbers" do
     def base_seven(number)
-        "0"
+        if number == 0
+            "0"
+        else
+            "1"
+        end
     end
     example "base seven representation of 0 is 0" do
         expect(base_seven(0)).to eq("0")
     end
-    example "base seven representation of 1 is 1"
+    example "base seven representation of 1 is 1" do
+        expect(base_seven(1)).to eq("1")
+    end
     example "base seven representation of 2 is 2"
     example "base seven representation of 3 is 3"
     example "base seven representation of 4 is 4"
