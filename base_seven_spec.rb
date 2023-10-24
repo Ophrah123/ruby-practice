7def9e1e83745bb442f06e3db0bfd0036cce02c3
Ophrah Adderley <ophrah.adderley@simplybusiness.com>
2023-10-24 11:09:32 -0400

    used parameterized tests

--- base_seven_spec.rb
+++ base_seven_spec.rb
@@ -2,7 +2,8 @@
 
 describe "base seven representation of numbers" do
     def base_seven(number)
-        number.to_s
+        return "10" if number == 7
+        number.to_s 
     end
     array=[0,1,2,3,4,5,6]
     array.each do | number |
@@ -11,8 +12,8 @@ describe "base seven representation of numbers" do
       end
     end
 
-    example "base seven representation of numbers 0 through 6 is the number itself" do
-        number=0
-        expect(base_seven(number)).to eq(number.to_s)
+    example "base seven representation of 7 is 10" do
+        number=7
+        expect(base_seven(number)).to eq("10")
     end
 end
