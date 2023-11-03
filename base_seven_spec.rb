c612aeb1cbcaebd6be450eec8631f11be0dbbaf9
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-11-03 19:08:10 +0000

    Refactor/Rename Variable: improved the name of a variable.

--- base_seven_spec.rb
+++ base_seven_spec.rb
@@ -3,8 +3,8 @@
 describe "base seven representation of numbers" do
     def base_seven(number)
         if (Math.log(number, 7) % Math.log(7, 7)).zero?
-            x = (Math.log(number, 7) / Math.log(7, 7)).to_i
-            (10**x).to_s
+            index = (Math.log(number, 7) / Math.log(7, 7)).to_i
+            (10**index).to_s
         elsif number % 7 == 0
             (10 * number / 7).to_s
         elsif number == 8
